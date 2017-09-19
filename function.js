312a495994c48e0deee24b80c3f0dacdf0d460d8
Austin Tanner <austin.tanner04@gmail.com>
2017-09-19 02:31:15 -0500

    changed a function name to make more sense, added comments for explanation later

--- function.js
+++ function.js
@@ -5,21 +5,22 @@ function removeTransition(x){
         return;
     }
     this.classList.remove('playing');
+    //transitions back to normal
 }
 
-function playSound(x){
+function onPress(x){
     const audio = document.querySelector(`audio[data-key="${x.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${x.keyCode}"]`);
 
     if(!audio){
         return;
     }
+
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
     key.addEventListener('transitionend', removeTransition);
+    //waits for transition to end then makes it the original size
 }
 
-window.addEventListener('keydown', playSound);
-//const keys = document.querySelectorAll('.key');
-//keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', onPress);
